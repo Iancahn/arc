0f4cb0c301f2af890b53147f0b9e50b185d346f4
Ian Cahn <iancahn@live.com>
2022-09-15 23:31:43 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+//ARC DEMO TEST
+var A = [1, 2, 3, 5, 6, 101, 8, 9, 11];
+
+
+function solution(A) {
+    let j = 1;
+    // write your code in JavaScript (Node.js 8.9.4)
+    let sortedArray = A.sort(function (a, b) { return a - b });
+    console.log(sortedArray);
+    for (let i = 0; i < sortedArray.length; i++) {
+        if (j < sortedArray[i]) {
+            console.log("Smallest number is: " + (j));
+            return j;
+        } else {
+            console.log("i is:" + sortedArray[i]);
+            console.log("j is :" + j);
+            j++;
+        }
+    }
+}
+
+solution(A);
+
+// This is a demo task.
+
+// Write a function:
+
+// function solution(A);
+
+// that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+// Given A = [1, 2, 3], the function should return 4.
+
+// Given A = [−1, −3], the function should return 1.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [1..100,000];
+// each element of array A is an integer within the range [−1,000,000..1,000,000].
